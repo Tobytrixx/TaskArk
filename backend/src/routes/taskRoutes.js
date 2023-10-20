12425c7c4d73ca9c665371aa3ae26afa7d76929e
tobytrixx <tobytrixx@gmail.com>
2023-10-20 20:10:37 +0100

    backend repush

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Import the relevant controller functions
 import { createTask, getTask, updateTask, deleteTask } from '../controllers/taskController.js';
 
-// Define the API endpoints
+// Define the task API endpoints
 router.post('/', createTask);
 router.get('/tasks/:id', getTask);
 router.patch('/tasks/:id', updateTask);
